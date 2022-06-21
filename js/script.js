c7574e10f5a7606a1cbda1afca08190eec4ed0cb
Poonam Kumbhar <poonamindu@gmail.com>
2022-06-21 15:20:16 +0530

    Updated

--- js/script.js
+++ js/script.js
@@ -57,70 +57,49 @@ navSlide();
 // view gallary
 
 
-/*
-	We start our code with an ajax request to fetch the data
-	from the json file.
-*/
-// First i create a new xmlhttp-request object.
-let http = new XMLHttpRequest();
-// the variable http holds now all methods and properties of the objct.
+// let http = new XMLHttpRequest();
 
-//  next i prepare the request with the open() method.
-http.open('get', '../json/gallary.json', true);
-// the first argument sets the http method.
-// in the second argument we pass the file where our data lives.
-// and last the keyword true, sets the request to be async.
+// http.open('get', '../json/gallary.json', true);
 
-// next i will send the request.
-http.send();
+// http.send();
 
-// Now i have to catch the response.
-// i will check the onload eventlistener.
-http.onload = function(){
-	// Inside the function i need to check the readystate and status properties.
-	if(this.readyState == 4 && this.status == 200){
-		// if we have a successful response, i have to parse the json data
-		// and convert them to a javascript array.
-		let gallarys = JSON.parse(this.responseText);
+// http.onload = function(){
+// 	if(this.readyState == 4 && this.status == 200){
+// 		let gallarys = JSON.parse(this.responseText);
 
-		// next i need an empty variable to add the incoming data.
-		let output = "";
+// 		let output = "";
 
-		// now i have to loop trough the products, and in every iteration
-		// i add an html template to the output variable.
-		for(let item of gallarys){
-			output += `
-				<div class="product">
-					<img src="${item.image}" alt="${item.description}">
+// 		for(let item of gallarys){
+// 			output += `
+// 				<div class="product">
+// 					<img src="${item.image}" alt="${item.description}">
 					
-					 <p class="description">${item.description}</p>
+// 					 <p class="description">${item.description}</p>
 					
 					
-				</div>
-			`;
-		}
-		/* and last i target the products container and add the data that the
-		output variable holds. */
-		document.querySelector(".gallarys").innerHTML = output;
-	}
+// 				</div>
+// 			`;
+// 		}
+// 		document.querySelector(".gallarys").innerHTML = output;
+// 	}
 	
-}
-
-
-// async function getPictures(){
-//   const result = await fetch("../json/gallary.json");
-//             const data = await result.json();
-//             const pics = data.pictures;
-            
-//             for(let item of pics){
-//               console.log("byy");
-//               var product = document.createElement("div");
-//               var img = document.createElement("img");
-//               img.src = item.image;
-//               product.appendChild(img);
-//               document.querySelector(".gallarys").appendChild(product);
-//             }
 // }
+
+
+async function getPictures(){
+  const result = await fetch("../json/gallary.json");
+            const data = await result.json();
+            const pics = data.pictures;
+            
+            for(let item of pics){
+              console.log("byy");
+              var product = document.createElement("div");
+              var img = document.createElement("img");
+              img.src = item.image;
+              product.appendChild(img);
+              document.querySelector(".gallarys").appendChild(product);
+            }
+}
 
  
 
